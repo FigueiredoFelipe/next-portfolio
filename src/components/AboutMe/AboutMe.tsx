3e16985513cc537879c1fc66db3e57a723fa9e99
Felipe <felipenegreirosadv@gmail.com>
2023-06-24 11:57:07 -0300

    fix: projects-component

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -6,17 +6,17 @@ const AboutMe = forwardRef<HTMLElement>((_, ref) => {
   return (
     <section className="py-16" ref={ref}>
       <div className="container m-auto justify-center w-full">
-        <div className="flex flex-col md:flex-row max-w-full justify-center">
+        <main className="flex flex-col md:flex-row max-w-full justify-center">
           <div className="container mx-auto max-w-full flex flex-col md:flex-row w-full md:w-8/12 px-4">
-            <div className="flex w-full justify-center items-center md:order-2 p-6">
+            <figure className="flex w-full justify-center items-center md:order-2 p-6">
               <Image
                 className="rounded-lg h-auto max-h-56"
                 src={ho1Image}
                 alt="home office desk"
                 priority={false} // {false} | {true}
               />
-            </div>
-            <div className="max-w-lg md:order-1">
+            </figure>
+            <article className="max-w-lg md:order-1">
               <h1 className="text-blue-500 font-extrabold text-lg">ABOUT ME</h1>
               <h4 className="font-extrabold text-2xl">
                 A dedicated Front-end React Developer based in Belo Horizonte,
@@ -32,9 +32,9 @@ const AboutMe = forwardRef<HTMLElement>((_, ref) => {
                 also a team player who thrives in collaborating with
                 cross-functional teams to produce outstanding web applications.
               </p>
-            </div>
+            </article>
           </div>
-        </div>
+        </main>
       </div>
     </section>
   );
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,18 +1,19 @@
 import { forwardRef } from "react";
 import { Projects } from "./Projects";
+import { Projects2 } from "./Projects2";
 
 const Portfolio = forwardRef<HTMLElement>((_, ref) => {
   return (
-    <section className="py-16 bg-gray-100" ref={ref}>
-      <main className="container flex flex-col m-auto w-full px-4 md:w-8/12">
-        <h1 className="text-blue-500 font-extrabold text-lg m-auto w-full md:w-8/12">
+    <section className="font-Poppins text-gray-800 py-16 bg-gray-100" ref={ref}>
+      <main className="container m-auto justify-center w-full">
+        <h1 className="text-blue-500 font-extrabold text-lg m-auto w-full md:w-8/12 px-5">
           PORTFOLIO
         </h1>
-        <h4 className="font-extrabold text-2xl w-full md:w-8/12 m-auto">
+        <h4 className="font-extrabold text-2xl w-full md:w-8/12 m-auto px-5 ">
           Each project is a unique piece of development🧩
         </h4>
         <Projects />
-        <Projects />
+        <Projects2 />
       </main>
     </section>
   );
