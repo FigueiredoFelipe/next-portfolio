11e132fa64990bb5994a5504a5244cf55c82b26f
Felipe <felipenegreirosadv@gmail.com>
2023-06-24 10:30:27 -0300

    fix: projects-component
    max-w

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -4,9 +4,11 @@ import { Projects } from "./Projects";
 const Portfolio = forwardRef<HTMLElement>((_, ref) => {
   return (
     <section className="py-16 bg-gray-100" ref={ref}>
-      <main className="container m-auto max-w-full w-full md:w-[1024px] flex flex-col px-4">
-        <h1 className="text-blue-500 font-extrabold text-lg">PORTFOLIO</h1>
-        <h4 className="font-extrabold text-2xl">
+      <main className="container flex flex-col m-auto w-full px-4 md:w-8/12">
+        <h1 className="text-blue-500 font-extrabold text-lg m-auto w-full md:w-8/12">
+          PORTFOLIO
+        </h1>
+        <h4 className="font-extrabold text-2xl w-full md:w-8/12 m-auto">
           Each project is a unique piece of development🧩
         </h4>
         <Projects />
--- src/components/Portfolio/Projects.tsx
+++ src/components/Portfolio/Projects.tsx
@@ -6,7 +6,7 @@ import agtvw610 from "../UI/img/agtvw610.png";
 
 export function Projects() {
   return (
-    <main className="m-auto max-w-full flex flex-col md:flex-row w-full md:w-full py-4 items-center space-x-24">
+    <main className="max-w-full md:w-8/12 flex flex-col md:flex-row w-full py-4 items-center md:space-8/12">
       <figure className="w-full">
         <Image
           className="rounded-lg w-full"
@@ -15,8 +15,8 @@ export function Projects() {
           priority={false} // {false} | {true}
         />
       </figure>
-      <aside className="flex flex-col w-3/5 justify-center items-center order-0 md:order-1 text-center mx-auto">
-        <h1 className="font-bold py-2">ArenaGamingTV</h1>
+      <aside className="flex flex-col md:w-3/5 justify-center items-center order-0 md:order-1 text-center w-auto">
+        <h1 className="font-bold pt-4 pb-2">ArenaGamingTV</h1>
         <h4 className="">
           Collaborated with Arena Gaming TV website, developed features like
           countdown timer, news section, store system, live stream list,
@@ -30,11 +30,11 @@ export function Projects() {
         <h5 className="font-bold cursor-pointer py-2">PHP, HTML, CSS</h5>
 
         <div className="flex flex-row w-full items-center justify-center">
-          <h5 className="font-bold py-2 flex justify-center items-center w-1/2 space-x-1">
+          <h5 className="font-bold py-2 flex justify-center items-center w-1/2 md:space-x-1">
             <span className="cursor-pointer">Code</span>
             <FaGithub className="cursor-pointer" />
           </h5>
-          <h5 className="font-bold py-2 flex justify-center items-center w-1/2 text-sm space-x-1">
+          <h5 className="font-bold py-2 flex justify-center items-center w-1/2 text-sm md:space-x-1">
             <span className="cursor-pointer">Live Demo</span>
             <HiOutlineExternalLink className="cursor-pointer" />
           </h5>
