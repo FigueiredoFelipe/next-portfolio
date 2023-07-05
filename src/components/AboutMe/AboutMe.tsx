bf8b1a5415f508c9311792b7c20858beb78aa9af
Felipe <felipenegreirosadv@gmail.com>
2023-07-04 21:20:19 -0300

    fix: changed-about-text

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -24,13 +24,14 @@ const AboutMe = forwardRef<HTMLElement>((_, ref) => {
               </h4>
               <p className="text-gray-500 pt-3 md:py-3">
                 As a Front-End Developer, I possess an impressive arsenal of
-                skills in HTML, CSS, JavaScript, React, and Tailwind. I excel in
-                designing and maintaining responsive websites that offer a
-                smooth user experience. My expertise lies in crafting dynamic,
-                engaging interfaces through writing clean and optimized code and
-                utilizing cutting-edge development tools and techniques. I am
-                also a team player who thrives in collaborating with
-                cross-functional teams to produce outstanding web applications.
+                skills in HTML, CSS, JavaScript, TypeScript, React, Tailwind,
+                and more. I excel in designing and maintaining responsive
+                websites that offer a smooth user experience. My expertise lies
+                in crafting dynamic, engaging interfaces through writing clean
+                and optimized code and utilizing cutting-edge development tools
+                and techniques. I am also a team player who thrives in
+                collaborating with cross-functional teams to produce outstanding
+                web applications.
               </p>
             </article>
           </div>
