2960da7774188672adf8a4282e652c8f2348bd64
Felipe <felipenegreirosadv@gmail.com>
2023-07-07 20:51:36 -0300

    feat: improved-contact-style
    also improved-footer-style

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -13,23 +13,28 @@ function Contact(_: any, ref: React.Ref<HTMLDivElement>) {
             <h2 className="font-bold text-2xl text-center md:text-start">
               Don&apos;t be shy! Hit me up! 👇
             </h2>
-            <div className="flex flex-wrap py-6">
-              <div className="flex items-center w-full md:w-auto ">
-                <FaMapMarkedAlt className="fill-blue-500 text-3xl" />
+            <div className="flex flex-wrap py-6 gap-10">
+              <div className="flex items-center w-full md:w-auto pl-7 md:pl-0">
+                <span className="bg-white h-16 w-16 rounded-full justify-center items-center flex shadow-custom">
+                  <FaMapMarkedAlt className="fill-blue-500 text-3xl" />
+                </span>
                 <div className="ml-2">
                   <h3 className="font-bold text-lg">Location</h3>
-                  <p className="cursor-pointer hover:text-blue-500">
+                  <p className="cursor-pointer hover:text-blue-700 duration-700 text-[#767676;]">
                     Belo Horizonte, Brazil
                   </p>
                 </div>
               </div>
-              <div className="flex items-center w-full md:w-auto p-0 md:p-4">
-                <FaMailBulk className="fill-blue-500 text-3xl shadow-custom" />
+
+              <div className="flex items-center w-full md:w-auto p-0 md:p-4 pl-7 md:pl-0">
+                <span className="bg-white h-16 w-16 rounded-full justify-center items-center flex shadow-custom">
+                  <FaMailBulk className="fill-blue-500 text-3xl" />
+                </span>
                 <div className="ml-2">
-                  <h3 className="font-bold text-base">Mail</h3>
+                  <h3 className="font-bold text-base">Email</h3>
                   <a
                     href="mailto:fjnfigueiredo@gmail.com"
-                    className="hover:text-blue-500"
+                    className="hover:text-blue-700 duration-700 text-[#767676;]"
                   >
                     fjnfigueiredo@gmail.com
                   </a>
