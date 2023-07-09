5d7636a843e0802b0ea4bc6e67f9b99d68a5fbfb
Felipe <felipenegreirosadv@gmail.com>
2023-07-09 11:38:56 -0300

    fix: improved-padding
    handling console errors

--- src/components/Portfolio/Project.tsx
+++ src/components/Portfolio/Project.tsx
@@ -36,10 +36,10 @@ export function Project({
             style={{ flexDirection: index % 2 ? "row" : "row-reverse" }}
             className="container mx-auto max-w-full md:flex w-full md:w-8/12 px-4 pb-7"
           >
-            <div className="flex flex-col md:flex-row gap-2 md:gap-12 h-auto md:h-[25rem] bg-white w-full p-8 shadow-custom rounded-2xl items-center py-10 pr-12">
+            <div className="flex flex-col md:flex-row gap-2 md:gap-12 h-auto md:h-[28rem] bg-white w-full p-8 shadow-custom rounded-2xl items-center py-10 pr-12">
               <figure className="w-full md:w-3/4 items-center justify-center">
                 <Image
-                  className="rounded-2xl shadow-custom h-auto overflow-hidden w-full md:w-[31rem]"
+                  className="rounded-2xl shadow-custom h-auto overflow-hidden w-full md:w-[30rem]"
                   src={imgSrc}
                   alt={imgAlt}
                   priority={imgPriority} // {false} | {true}
@@ -47,9 +47,8 @@ export function Project({
               </figure>
 
               <article className="flex flex-col h-auto justify-center text-center w-full md:w-[28rem] y-2 pr-6">
-                <h3 className="font-bold">
-                  <h4 className="font-extrabold text-lg">{title}</h4>
-                </h3>
+                <h3 className="font-bold"></h3>
+                <h4 className="font-extrabold text-lg pt-4 md:pt-0">{title}</h4>
                 <p className="text-gray-500 py-3 text-center md:text-justify font-medium text-base">
                   {details}
                 </p>
