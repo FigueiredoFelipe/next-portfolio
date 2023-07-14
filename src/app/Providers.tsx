d8efef08a43fd28fa93a1b904c0c06185290cfd7
Felipe <felipenegreirosadv@gmail.com>
2023-07-14 20:50:08 -0300

    fix: cleanned-code

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "next-themes";
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <>{children}</>
+      {children}
     </ThemeProvider>
   );
 };
