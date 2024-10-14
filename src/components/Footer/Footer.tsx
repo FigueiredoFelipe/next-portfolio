f17ca1387a866cbe60eacb4a600d5d76e557cba9
Felipe Figueiredo <fjnfigueiredo@gmail.com>
2024-10-14 09:46:26 -0300

    improvement: enhance Footer layout responsiveness and icon spacing

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,33 +1,31 @@
-import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 export function Footer() {
   return (
-    <>
-      <footer className="bg-gray-800 dark:bg-zinc-900">
-        <div className="container mx-auto flex justify-center max-w-full p-6 md:p-12">
-          <div className="max-w-full md:max-w-3/5">
-            <p className="text-white text-xs md:text-sm font-semibold">
-              Copyright © 2023. All rights reserved.
-            </p>
-          </div>
-          <div className="text-2xl text-white flex space-x-2 pl-6 md:pl-20">
-            <a
-              href="https://www.linkedin.com/in/fjnfigueiredo/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin className="fill-white hover:scale-125 duration-300" />
-            </a>
-            <a
-              href="https://github.com/FigueiredoFelipe/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithubSquare className="fill-white hover:scale-125 duration-300" />
-            </a>
-          </div>
+    <footer className="bg-gray-800 dark:bg-zinc-900">
+      <div className="container mx-auto flex justify-between items-center p-6 md:p-12">
+        <div className="text-white text-xs md:text-sm font-semibold">
+          <p>Copyright © 2023. All rights reserved.</p>
         </div>
-      </footer>
-    </>
+        <div className="text-2xl text-white flex space-x-4">
+          <a
+            href="https://www.linkedin.com/in/fjnfigueiredo/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn Profile"
+          >
+            <FaLinkedin className="fill-white hover:scale-125 transition-transform duration-300" />
+          </a>
+          <a
+            href="https://github.com/FigueiredoFelipe/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub Profile"
+          >
+            <FaGithubSquare className="fill-white hover:scale-125 transition-transform duration-300" />
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 }
